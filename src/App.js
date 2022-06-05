d278e24a7ef7fa458ff288a5f3996cf7b71a801f
Roman Shevchyshyn <romanshevchyshyn1998@gmail.com>
2022-06-05 21:57:11 +0300

    project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center my-4">
+        Rick and Morty <span className="text-primary">WiKi</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
